3bbab877de4dabf0abb2727a3e6c403e7db1027c
Mansen <50342436+mansen420@users.noreply.github.com>
2023-10-22 00:25:23 +0300

    Revert "Start to implement transformation matrix"
    This reverts commit bc47ab279cbf92d6cac7369d0e8bb33a26f6ee78.

--- Vector/VecOperator.java
+++ Vector/VecOperator.java
@@ -6,21 +6,23 @@ import static java.lang.Math.min;
 public final class VecOperator
 {
     private VecOperator()
-    { //static class
+    {
     }
-    //color
     public static Color mulColor (Color u, Color v)
-    {   //returns color equal to "scaling" u by v, clipping at 255
+    {   //returns color equal to scaling u by v, clipping to 255
         return new Color( min(u.getRed()*v.getRed()/255, 255),  min(u.getGreen()*v.getGreen()/255, 255), min(u.getBlue()*v.getBlue()/255, 255));
     }
     public static Color sumColor (Color u, Color v)
-    {   //returns plain element wise sum, clipping at 255
+    {   //returns plain sum clipping to 255
         return new Color(min (u.getRed()+v.getRed(), 255), min(u.getGreen()+v.getGreen(), 255), min(u.getBlue()+v.getBlue(), 255));
     }
-    //vector
     public static Vec3f minus (Vec3f u, Vec3f v)
     {   //non ideal implementation
         return new Vec3f(u.x()-v.x(), u.y()-v.y(), u.z()-v.z());
+    }
+    public static float dot (Vec3f u, Vec3f v)
+    {
+        return u.x()*v.x()+u.y()*v.y()+u.z()*v.z();
     }
     public static  <V extends Vector> float dot (V u, V v)
     {
@@ -39,59 +41,5 @@ public final class VecOperator
     public static Pixel transpose(Pixel p)
     {
        return new Pixel(p.y(), p.x());
-    }
-    //matrix
-    public static Matrix mul(Matrix l, Matrix r)
-    {   //TODO error handling : throw error here
-        if (l.nColumns()!=r.nRows())
-        {
-            //throw error
-            System.out.println("Error : incompatible matrices");
-        }
-        Matrix resultMatrix = new Matrix(l.nRows(), r.nColumns());
-        for (int i = 0; i< l.nRows(); i++)
-        {
-            for (int j = 0; j<r.nColumns();j++)
-            {   //init value with 0
-                float sum = 0;
-                //add dot product of left row and right column
-                for (int k = 0; k<l.nColumns();k++)
-                {
-                    sum += l.getElement(i, k)*r.getElement(k,j);
-                }
-                resultMatrix.setElement(i, j, sum);
-            }
-        }
-        return resultMatrix;
-    }
-    public static Matrix lookAt(Vec3f eye, Vec3f center, Vec3f up)
-    {
-        Vec3f z = minus(eye, center);
-        Vec3f x = cross(up, z);
-        Vec3f y = cross(z, x);
-        Matrix minv = Matrix.getIdentityMatrix(4);
-        Matrix tr = Matrix.getIdentityMatrix(4);
-        for (int i = 0; i<3; i++)
-        {
-            minv.setElement(0, i, x.raw[i].floatValue());
-            minv.setElement(1, i, y.raw[i].floatValue());
-            minv.setElement(2, i, z.raw[i].floatValue());
-            tr.setElement(i, 3, -eye.raw[i].floatValue());
-        }
-        return mul(minv, tr);
-    }
-    public static int depth = 255;
-    public static Matrix viewport (int x, int y, int w, int h)
-    {
-        Matrix resultMatrix = Matrix.getIdentityMatrix(4);
-        resultMatrix.setElement(0,3, x+w/2.f);
-        resultMatrix.setElement(1,3, y+h/2.f);
-        resultMatrix.setElement(2,3, depth/2.f);
-
-        resultMatrix.setElement(0,0, w/2);
-        resultMatrix.setElement(1,1, h/2);
-        resultMatrix.setElement(2,2, h/2);
-
-        return resultMatrix;
     }
 }
