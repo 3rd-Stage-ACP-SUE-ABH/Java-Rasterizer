7f725521946a8598d4eb38511d18d0de432c3347
bannads <bannaziad990@gmail.com>
2023-11-03 00:18:39 +0300

    restructured: mvc

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 import java.awt.*;
 import java.io.IOException;
 
+import model.pipeline.programmable.*;
+import model.renderer.*;
+import view.Window;
 
-import pipeline.programmable.*;
-import renderer.*;
-import window.Window;
+import static model.math.VecOperator.*;
 
-import static math.VecOperator.*;
-public class Main
-{
-    public static void main(String[]args) throws IOException
-    {
-        //init renderer
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // init renderer
         Renderer myRenderer = new Renderer(pix_width, pix_height);
         myRenderer.readTexture("C:/Users/msi/Desktop/african_head_diffuse.png");
         PhongShader myGShader = new PhongShader();
@@ -20,33 +18,33 @@ public class Main
         Window myWindow = new Window("Java Rasterizer", myRenderer);
 
         int i = 0;
-        //TODO structure : make breaking of while loop dependent on user input
-        //render loop
-        while (true)
-        {
+        // TODO structure : make breaking of while loop dependent on user input
+        // render loop
+        while (true) {
             long start = System.nanoTime();
-            //clear buffers at the beginning of the frame. 10-15ms
+            // clear buffers at the beginning of the frame. 10-15ms
             myRenderer.myRasterizer.clearDepthBuffer();
-            myRenderer.myRasterizer.clearPixelBuffer(new Color(50,50,50));
+            myRenderer.myRasterizer.clearPixelBuffer(new Color(50, 50, 50));
 
-            //specify rotation angle of object around y-axis in radians
-            rotationAngle = (float)i/5;
+            // specify rotation angle of object around y-axis in radians
+            rotationAngle = (float) i / 5;
 
-            //do the magic
+            // do the magic
             myGShader.rotationAngle = rotationAngle;
             myGShader.updateMatrices();
             myRenderer.renderModel();
 
-            //write to the buffer after doing all processing.
-            //avoid writing multiple times per frame as tearing happens.
+            // write to the buffer after doing all processing.
+            // avoid writing multiple times per frame as tearing happens.
             myRenderer.printBuffer();
             myWindow.update();
             i++;
 
             long time = System.nanoTime() - start;
-            System.out.println("processing time :  " + (((double) time/1_000_000) + " ms/frame"));
+            System.out.println("processing time :  " + (((double) time / 1_000_000) + " ms/frame"));
         }
     }
+
     public static final int pix_height = 300;
     public static final int pix_width = 300;
 }
--- Model/Model.java
+++ Model/Model.java
@@ -1,9 +1,10 @@
-package Model;
+package model;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Model {
     private final ArrayList<float[]> vertexCoords;
     private final ArrayList<float[]> normalCoords;
@@ -15,8 +16,9 @@ public class Model {
     private final ArrayList<int[]> Nindices;
     private final ArrayList<int[]> Tindices;
     private final String filePath;
-    public float minCoord=Float.MAX_VALUE;
-    public float maxCoord=-Float.MAX_VALUE;
+    public float minCoord = Float.MAX_VALUE;
+    public float maxCoord = -Float.MAX_VALUE;
+
     public Model(String filePath) {
         this.filePath = filePath;
         normalIndices = new ArrayList<>();
@@ -25,97 +27,89 @@ public class Model {
         texCoords = new ArrayList<>();
         normalCoords = new ArrayList<>();
         vertexCoords = new ArrayList<>();
-        //load data AFTER instantiating the fields
+        // load data AFTER instantiating the fields
         loadData();
         Vindices = new ArrayList<>();
         Nindices = new ArrayList<>();
         Tindices = new ArrayList<>();
         organizeIndices();
     }
-    public void printFaces()
-    {
-        for (int i =0; i<Vindices.size();i++)
-        {
-            for (int j = 0;j<3;j++)
-            {
+
+    public void printFaces() {
+        for (int i = 0; i < Vindices.size(); i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(Vindices.get(i)[j]);
                 if (!Tindices.isEmpty())
-                    System.out.print("/"+Tindices.get(i)[j]);
+                    System.out.print("/" + Tindices.get(i)[j]);
                 if (!Nindices.isEmpty())
-                    System.out.print("/"+Nindices.get(i)[j]);
+                    System.out.print("/" + Nindices.get(i)[j]);
                 System.out.print(" ");
             }
             System.out.println();
         }
     }
-    private void organizeIndices ()
-    {       //inserting data into arrays of 3 rather than having everything in one array
-        if (vertexIndices.size()%3==0)
-        {
-            for (int i =0; i<vertexIndices.size(); i++)
-            {
-                Vindices.add(new int[]{vertexIndices.get(i++), vertexIndices.get(i++), vertexIndices.get(i)});
+
+    private void organizeIndices() { // inserting data into arrays of 3 rather than having everything in one array
+        if (vertexIndices.size() % 3 == 0) {
+            for (int i = 0; i < vertexIndices.size(); i++) {
+                Vindices.add(new int[] { vertexIndices.get(i++), vertexIndices.get(i++), vertexIndices.get(i) });
                 if (!normalIndices.isEmpty())
-                    Nindices.add(new int[]{normalIndices.get(i-2), normalIndices.get(i-1), normalIndices.get(i)});
+                    Nindices.add(
+                            new int[] { normalIndices.get(i - 2), normalIndices.get(i - 1), normalIndices.get(i) });
                 if (!textureIndices.isEmpty())
-                    Tindices.add(new int[]{textureIndices.get(i-2), textureIndices.get(i-1), textureIndices.get(i)});
+                    Tindices.add(
+                            new int[] { textureIndices.get(i - 2), textureIndices.get(i - 1), textureIndices.get(i) });
             }
-        }
-        else
-        {
+        } else {
             System.out.println("Critical error : faces not divisible by 3");
         }
     }
 
-    public boolean hasTextureCoords()
-    {
+    public boolean hasTextureCoords() {
         return !Tindices.isEmpty();
     }
-    public boolean hasNormalCoords()
-    {
+
+    public boolean hasNormalCoords() {
         return !Nindices.isEmpty();
     }
 
-
-    public float[] getVertexCoords(int idx)
-    {
+    public float[] getVertexCoords(int idx) {
         return vertexCoords.get(idx);
     }
-    public float[] getTexCoords(int idx)
-    {
+
+    public float[] getTexCoords(int idx) {
         return texCoords.get(idx);
     }
-    public float[] getNormalCoords(int idx)
-    {
+
+    public float[] getNormalCoords(int idx) {
         return normalCoords.get(idx);
     }
-    public int[] getVertexIndices(int idx)
-    {
+
+    public int[] getVertexIndices(int idx) {
         return Vindices.get(idx);
     }
-    public int[] getNormalIndices(int idx)
-    {
+
+    public int[] getNormalIndices(int idx) {
         return Nindices.get(idx);
     }
-    public int[] getTextureIndices(int idx)
-    {
+
+    public int[] getTextureIndices(int idx) {
         return Tindices.get(idx);
     }
 
-    public int nVerts()
-    {
+    public int nVerts() {
         return vertexCoords.size();
     }
-    public int nNormals()
-    {
+
+    public int nNormals() {
         return normalCoords.size();
     }
-    public int nTextures()
-    {
+
+    public int nTextures() {
         return texCoords.size();
     }
-    public int nFaces()
-    {
+
+    public int nFaces() {
         return Vindices.size();
     }
 
@@ -136,53 +130,49 @@ public class Model {
                     float x = Float.parseFloat(P[1]);
                     float y = Float.parseFloat(P[2]);
                     float z = Float.parseFloat(P[3]);
-                    float minCandidate = Math.min(x,Math.min(y,z));
+                    float minCandidate = Math.min(x, Math.min(y, z));
                     minCoord = Math.min(minCoord, minCandidate);
-                    float maxCandidate = Math.max(x,Math.max(y,z));
+                    float maxCandidate = Math.max(x, Math.max(y, z));
                     maxCoord = Math.max(maxCoord, maxCandidate);
-                    vertexCoords.add(new float[]{x, y, z});
+                    vertexCoords.add(new float[] { x, y, z });
                 }
                 if (P[0].equals("vn")) {
                     float x = Float.parseFloat(P[1]);
                     float y = Float.parseFloat(P[2]);
                     float z = Float.parseFloat(P[3]);
-                    float[] Vs = {x, y, z};
+                    float[] Vs = { x, y, z };
                     normalCoords.add(Vs);
                 }
                 if (P[0].equals("vt")) {
                     float x = Float.parseFloat(P[1]);
                     float y = Float.parseFloat(P[2]);
                     float z = Float.parseFloat(P[3]);
-                    float[] Vs = {x, y, z};
+                    float[] Vs = { x, y, z };
                     texCoords.add(Vs);
                 }
                 String[] parts;
 
-                if (P[0].equals("f"))
-                {
-                    //TODO bug fixing :  as far as I'm aware this condition is unnecessary,
+                if (P[0].equals("f")) {
+                    // TODO bug fixing : as far as I'm aware this condition is unnecessary,
                     // and can miss faces in some formats. Check with team.
-                    if (true)   // previous condition : P[1].contains("/")
+                    if (true) // previous condition : P[1].contains("/")
                     {
-                        for (int i = 1; i < P.length; i++)
-                        {
+                        for (int i = 1; i < P.length; i++) {
                             int coordCounter = 0;
                             parts = P[i].split("/"); // '1 1 1'
-                            //-1 because indices in .obj format start from 1
-                            vertexIndices.add(Integer.parseInt(parts[coordCounter++])-1);
+                            // -1 because indices in .obj format start from 1
+                            vertexIndices.add(Integer.parseInt(parts[coordCounter++]) - 1);
                             if (!texCoords.isEmpty())
-                                textureIndices.add(Integer.parseInt(parts[coordCounter++])-1);
+                                textureIndices.add(Integer.parseInt(parts[coordCounter++]) - 1);
                             if (!normalCoords.isEmpty())
-                                 normalIndices.add(Integer.parseInt(parts[coordCounter++])-1);
+                                normalIndices.add(Integer.parseInt(parts[coordCounter++]) - 1);
                         }
                     }
                 }
 
             }
             reader.close();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
 
