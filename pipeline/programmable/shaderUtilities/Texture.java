e87e3856de223d50383a3230217e72d1d39fef5d
Mansen <50342436+mansen420@users.noreply.github.com>
2023-11-03 16:15:51 +0300

    Reorganize files

---
+++ pipeline/programmable/shaderUtilities/Texture.java
@@ -0,0 +1,33 @@
+package pipeline.programmable.shaderUtilities;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Texture {
+    public Texture(String filePath) throws IOException {readTexture(filePath);}
+    private Color[] textureData;
+    private int texHeight, texWidth;
+    public int getWidth() { return texWidth; }
+    public int getHeight() { return texHeight; }
+    public Color getPixel(int x, int y) { return textureData[x+y*texWidth]; }
+    public Color[] readTexture(String filePath) throws IOException
+    {
+        //loads texture data from a .png, inverted vertically
+        //TODO error handling : try catch
+        BufferedImage textureReader = ImageIO.read(new File(filePath));
+        texHeight= textureReader.getHeight();
+        texWidth=textureReader.getWidth();
+        textureData = new Color[textureReader.getHeight()*textureReader.getWidth()];
+        for (int i =textureReader.getHeight()-1;i>=0;i--)
+        {
+            for (int j =0; j<textureReader.getWidth();j++)
+            {
+                textureData[(textureReader.getHeight()-i-1)*textureReader.getWidth()+j]=new Color(textureReader.getRGB(j,i));
+            }
+        }
+        return textureData;
+    }
+}
